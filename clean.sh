ce90e75b6ebbad94daa19fda981f56f68ee02e38
kgfathur <sahabatsolder@gmail.com>
2020-11-17 13:01:20 +0700

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf /data/grafana/*
+sudo rm -rf /data/prometheus0/*
+sudo rm -rf /data/prometheus1/*
+sudo rm -rf /data/prometheus2/*
+sudo rm -rf /data/prometheus0/.*
+sudo rm -rf /data/prometheus1/.*
+sudo rm -rf /data/prometheus2/.*
+
+sudo rm -rf /data/sidecar0/*
+sudo rm -rf /data/sidecar1/*
+sudo rm -rf /data/sidecar2/*
+sudo rm -rf /data/sidecar0/.*
+sudo rm -rf /data/sidecar1/.*
+sudo rm -rf /data/sidecar2/.*
